a786e4bfbfa31aafb875269990f9c3c7c0cc2f9e
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-11-07 13:38:13 -0500

    switched observer from textNode to classList

--- content_script.js
+++ content_script.js
@@ -15,20 +15,42 @@ const statusText = document.getElementById("state-text");
 const config = { attributes: true, childList: true, subtree: true };
 
 const callback = (mutationList, observer) => {
-  if (mutationList[0].type === "childList") {
-    const statusTextValue = document.getElementById("state-text").textContent;
-    if (
-      statusTextValue === "Not Ready - Phone Failure" ||
-      statusTextValue === "Not Ready - Phone Working" ||
-    ) {
-      interval = setInterval(playBeep, 5000);
-    } else {
-      clearInterval(interval);
-      stopBeep();
+  for (const mutation of mutationList) {
+    if (mutation.type === "attributes" && mutation.attributeName === "class") {
+      const statusTextClasses = document.getElementById("state-text").classList;
+      if (statusTextClasses.contains("yellowText")) {
+        console.log("Phone down");
+        interval = setInterval(playBeep, 5000);
+      } else {
+        clearInterval(interval);
+        console.log("Phone up");
+        stopBeep();
+      }
     }
   }
 };
 
+let observing = false;
+
 const observer = new MutationObserver(callback);
 
-observer.observe(statusText, config);
+const parentDiv = document.getElementById("signout-area");
+let notifyButton = document.createElement("button");
+notifyButton.textContent = "Notify";
+notifyButton.classList.add("btn");
+notifyButton.classList.add("btn-default");
+
+parentDiv.appendChild(notifyButton);
+
+notifyButton.addEventListener("click", () => {
+  if (!observing) {
+    observer.observe(statusText, config);
+    observing = true;
+    notifyButton.textContent = "Stop Notifying";
+  } else {
+    observer.disconnect();
+    observing = false;
+    stopBeep();
+    notifyButton.textContent = "Notify";
+  }
+});
